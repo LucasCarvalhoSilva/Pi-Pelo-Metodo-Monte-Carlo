083520b743c32ab5a9175f05f902dd873edc0006
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2024-12-21 08:18:20 -0300

    tracking execution time

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from src.calculator import calculatePi
 
+import time
+
 def main():
-  print("=> Cálculo de Pi pelo Método de Monte Carlo<=")
+  print("=> Cálculo de Pi pelo Método de Monte Carlo <=")
 
   try:
+    startTime = time.time()
     numberOfPoints = int(input("Digite a quantidade de pontos que devem ser simulados: "))
     if numberOfPoints <= 0:
       raise ValueError("Digite um valor valído, precisa ser um número maior do que 0")
@@ -13,6 +16,12 @@ def main():
   
   estimatedPi = calculatePi(numberOfPoints)
   print(f"Com {numberOfPoints} pontos, o valor estimado de Pi é {estimatedPi:.6f}")
+  endTime = time.time()
+
+  executionTime = (endTime - startTime) * 1000
+  print("Tempo de execução =>", executionTime)
 
 if __name__ == "__main__":
   main()
+
+
